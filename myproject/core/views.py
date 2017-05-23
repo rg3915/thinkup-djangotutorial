24652eaf724ce9e51b925f6ab49d9659309c9a35
rg3915 <rg3915@yahoo.com.br>
2017-05-22 22:00:16 -0300

    Refatorando templates e Model dos filmes

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy as r
+from django.views.generic import CreateView, ListView, DetailView
+from django.db.models import Max
+from .models import Movie
 
 
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'landpage.html')
+
+
+class MovieList(ListView):
+    template_name = 'core/movie_list.html'
+    model = Movie
+    context_object_name = 'movies'
+
+    def get_queryset(self):
+        m = Movie.objects.all()
+        # Filme de maior bilheteria
+        if self.request.GET.get('great_movie', False):
+            q = Movie.objects.all().aggregate(Max('raised'))
+            m = Movie.objects.filter(raised=q['raised__max'])
+        return m
+
+
+class MovieDetail(DetailView):
+    template_name = 'core/movie_detail.html'
+    model = Movie
+
+
+class MovieCreate(CreateView):
+    template_name = 'core/movie_form.html'
+    model = Movie
+    fields = '__all__'
+    success_url = r('core:movie_list')
